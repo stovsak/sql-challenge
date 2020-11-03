4a6a49bf2e6ee8e067e65bbc19153abfe8f03103
STEVE OVSAK <steveovsak@MacBook-Pro.local>
2020-11-03 13:07:31 -0700

    additional analysis

---
+++ DataAnalysis6.sql
@@ -0,0 +1,10 @@
+/*
+List all employees in the Sales department including:
+employee number, last name, first name, and department name.
+*/
+
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees ON dept_emp.emp_no = employees.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
---
+++ DataAnalysis7.sql
@@ -0,0 +1,12 @@
+/*
+List all employees in the Sales and Development departments including: 
+employee number, last name, first name, and department name.
+*/
+
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees ON dept_emp.emp_no = employees.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE
+departments.dept_name = 'Sales'
+OR departments.dept_name = 'Development';
